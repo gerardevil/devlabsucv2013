a333137f25416b0a8b45520e4ec16f4c5c13f958
Your Name <you@example.com>
2013-08-15 06:28:19 +0000

    FROM WEBFACTION SERVER: Loading new Settings.py config file

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -59,16 +59,20 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/devlabsdb/webapps/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = "/assets/"
+STATIC_URL = '/static/'
+
+DEVLABS_ROOT = '/home/devlabsdb/webapps/programaciondocente/devlabsucv2013/'
 
 # Additional locations of static files
+
 STATICFILES_DIRS = (
-	os.path.join(os.getcwd(), "views/assets"),
-	#os.path.join(os.getcwd(), "views/assets/css"),
+	os.path.join(DEVLABS_ROOT, "views/assets").replace('\\', '/'),
+	#os.path.join(DEVLABS_ROOT, "views/assets/css").replace('\\', '/'),
+	#os.path.join(DEVLABS_ROOT, "views/assets/js").replace('\\', '/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(os.getcwd(), 'views').replace('\\', '/'),
+	os.path.join(DEVLABS_ROOT, 'views').replace('\\', '/'),
 )
 
 INSTALLED_APPS = (
@@ -121,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
-	
+
     #Non Django App: Team Apps
     'principal'
 )
