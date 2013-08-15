9c20b49e9849993cfbf6650e2f30856451bd102f
Gerardo Linares <gerardevil@gmail.com>
2013-08-15 02:14:44 -0430

    Servicios para CRUD de Materias

--- principal/views.py
+++ principal/views.py
@@ -13,24 +13,24 @@ def inicio(request):
 
 # Login v0 by using GET, must be by using CSRF by POST
 def login(request):
-    user_ext = request.GET['user']
-    password_ext = request.GET['password']
-
+	user_ext = request.GET['user']
+	password_ext = request.GET['password']
+	
 	if user_ext and password_ext:
-        try:
-            password = Usuario.objects.get(usuario_id=int(user_ext)).clave
-        except Exception, e:
+		try:
+			password = Usuario.objects.get(usuario_id=int(user_ext)).clave
+		except Exception, e:
             #return HttpResponse('<h2>User not found</h2>')
-            return render_to_response("Home.html",{'err':2})
-        else:
-                if password_ext == password:
-                    return render_to_response('Principal_Prof.html')
+			return render_to_response("Home.html",{'err':2})
+		else:
+				if password_ext == password:
+					return render_to_response('Principal_Prof.html')
                     #return render_to_response(<frontend_error_view>)
-                else:
+				else:
                     #return HttpResponse('<h2>Invalid password</h2>')
-                    return render_to_response("Home.html",{'err':3})
-    else:
-        return render_to_response("Home.html",{'err':1})
+					return render_to_response("Home.html",{'err':3})
+	else:
+		return render_to_response("Home.html",{'err':1})
 		
 		
 def listarMaterias(request):
