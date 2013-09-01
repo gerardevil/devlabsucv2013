aaad37123bc6876add5a84cec82d7d74368af946
Jesus Gomez <jesus.igp009@gmail.com>
2013-09-01 11:16:11 -0400

    Agrgando reinicio de Apache a deployment script

--- doc/deployment/deploy-script.sh
+++ doc/deployment/deploy-script.sh
@@ -4,15 +4,25 @@
 # --link    Create a symbolic link to each file instead of copying.
 # --noinput Do NOT prompt the user for input of any kind.
 
-echo "###################################"
-echo "# Actualizando repositporio local #"
-echo "###################################"
+echo "-------------------"
+echo "- Apache a dormir -"
+echo "-------------------"
+./../../../apache2/bin/stop
+
+echo "-----------------------------------"
+echo "- Actualizando repositporio local -"
+echo "-----------------------------------"
 git pull
-echo "####################################"
-echo "# Sincronizando Archivos estaticos #"
-echo "####################################"
+echo "------------------------------------"
+echo "- Sincronizando Archivos estaticos -"
+echo "------------------------------------"
 python2.7 ../../manage.py collectstatic -link
-echo "######################################"
-echo "# Todo bien (:  [Deployment Finised] #"
-echo "######################################"
+echo "--------------------------------------"
+echo "- Todo bien (:  [Deployment Finised] -"
+echo "--------------------------------------"
+
+echo "--------------------"
+echo "- Apache despierta -"
+echo "--------------------"
+./../../../apache2/bin/start
 exit 0
