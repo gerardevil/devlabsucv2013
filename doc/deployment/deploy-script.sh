81e93045a336d1585281fb2bbc1ea55cdb0cc837
Jesus Gomez <jesus.igp009@gmail.com>
2013-09-04 14:50:28 -0400

    Actualizando delployment script

--- doc/deployment/deploy-script.sh
+++ doc/deployment/deploy-script.sh
@@ -5,27 +5,27 @@
 # --noinput Do NOT prompt the user for input of any kind.
 
 echo "-------------------"
-echo "\e[1;31;40m - Apache STOP -"
+echo -e "\e[1;31;40m - Apache STOP -"
 echo "-------------------"
 ./../../../apache2/bin/stop
 
 echo "-----------------------------------"
-echo "-\e[1;31;40m Update local repository -"
+echo -e "-\e[1;31;40m Update local repository -"
 echo "-----------------------------------"
 git pull
 echo "------------------------------------"
-echo "-\e[1;31;40m Syncronize Static Files -"
+echo -e "-\e[1;31;40m Syncronize Static Files -"
 echo "------------------------------------"
 python2.7 ../../manage.py collectstatic -link
 
 echo "--------------------"
-echo "-\e[1;31;40m Apache START -"
+echo -e "-\e[1;31;40m Apache START -"
 echo "--------------------"
 ./../../../apache2/bin/start
 
 
 echo "--------------------------------------"
-echo "-\e[1;31;40m OK (:  [Deployment Finised] -"
+echo -e "-\e[1;31;40m OK (:  [Deployment Finised] -"
 echo "--------------------------------------"
 
 exit 0
