69e3cff94530122d02ff08cbcce5f94bfd6ca789
Jesus Gomez <jesus.igp009@gmail.com>
2013-09-04 15:06:43 -0400

    Cambiando deploy script

--- doc/deployment/deploy-script.sh
+++ doc/deployment/deploy-script.sh
@@ -4,28 +4,28 @@
 # --link    Create a symbolic link to each file instead of copying.
 # --noinput Do NOT prompt the user for input of any kind.
 
-echo "-------------------"
-echo -e "\e[1;31;40m - Apache STOP -"
-echo "-------------------"
+echo "---------------"
+echo "- Apache STOP -"
+echo "---------------"
 ./../../../apache2/bin/stop
 
-echo "-----------------------------------"
-echo -e "-\e[1;31;40m Update local repository -"
-echo "-----------------------------------"
+echo "---------------------------"
+echo "- Update local repository -"
+echo "---------------------------"
 git pull
-echo "------------------------------------"
-echo -e "-\e[1;31;40m Syncronize Static Files -"
-echo "------------------------------------"
+echo "--------------------------"
+echo " Syncronize Static Files -"
+echo "--------------------------"
 python2.7 ../../manage.py collectstatic -link
 
-echo "--------------------"
-echo -e "-\e[1;31;40m Apache START -"
-echo "--------------------"
+echo "----------------"
+echo "- Apache START -"
+echo "----------------"
 ./../../../apache2/bin/start
 
 
-echo "--------------------------------------"
-echo -e "-\e[1;31;40m OK (:  [Deployment Finised] -"
-echo "--------------------------------------"
+echo "-------------------------------"
+echo "- OK (:  [Deployment Finised] -"
+echo "-------------------------------"
 
 exit 0
