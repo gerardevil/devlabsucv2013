a0e70f3a3076380088b055dd403fc54aadbefb16
Gerardo Linares <gerardevil@gmail.com>
2013-09-14 02:35:04 -0430

    Avances tabla horarios

---
+++ static/js/horario.js
@@ -0,0 +1,88 @@
+
+$(function(){
+
+	cargarHorario();
+	//cargarProfesores();
+	//cargarMaterias();
+
+});
+
+
+
+function cargarHorario(){
+
+	$('#tablaHorario').hideLoading();
+	$('#tablaHorario').showLoading();
+	
+	var url = '/prueba/cc';
+	var param = '';
+	
+	$.ajax({  
+		 type: 'GET',  
+		 url: url,
+		 dataType: 'json',
+		 data: param,
+		 success: function(respuesta){
+			alert(respuesta);
+			alert(respuesta.length);
+			for(i=0;i<respuesta.length;i++){
+			
+				alert(respuesta[i+'']['nombre']);
+				
+			}
+			
+		 },
+		 complete: function(){
+			$('#tablaHorario').hideLoading();
+		 },
+		 error: function(err,errmsg){
+			alert(errmsg);
+		 }
+		});
+}
+
+function cargarProfesores(){
+
+	$('#tablaProfesores').hideLoading();
+	$('#tablaProfesores').showLoading();
+	
+	var url = '';
+	var param = '';
+	
+	$.ajax({  
+		 type: 'GET',  
+		 url: url,
+		 dataType: 'json',
+		 data: param,
+		 success: function(respuesta){
+			
+		 },
+		 complete: function(){
+			$('#tablaProfesores').hideLoading();
+		 }
+		});
+
+}
+
+function cargarMaterias(){
+
+	$('#tablaMaterias').hideLoading();
+	$('#tablaMaterias').showLoading();
+	
+	var url = '';
+	var param = '';
+	
+	$.ajax({  
+		 type: 'GET',  
+		 url: url,
+		 dataType: 'json',
+		 data: param,
+		 success: function(respuesta){
+			
+		 },
+		 complete: function(){
+			$('#tablaMaterias').hideLoading();
+		 }
+		});
+
+}
