0a23fc21f8d02fc4c6c86dd69b75b80ae80f1c51
Rafael Machado <mchdr19@gmail.com>
2013-09-16 23:00:52 -0430

    mostrando materias solicitadas

--- static/js/script_profesor.js
+++ static/js/script_profesor.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     var numero_horarios = 0
 
     $("#pg_am").hide()
+    $("#agregarHor").remove()
 
     $( "#id_materia" ).change(function() {
         $("#pg_am").show()
+        $("#agregarHor").remove()
         $(".horarios").remove()
         $(".horariosl").remove()
         $(".error_am").remove()
@@ -32,7 +34,8 @@ $(document).ready(function() {
                             //alert('<option value="'+i+'">'+d.dia_semana+" "+d.hora_inicio+" "+ d.hora_fin+'</option>')
                             $(".horarios").append('<option value="'+ d.valor+'">'+d.dia_semana+" "+d.hora_inicio+"-"+ d.hora_fin+'</option>')
                         }
-                        $("#agregarMat").append('</select></td></tr>')
+                        $("#agregarMat").append('</select>')
+                        $("#agregarMat").append('<button type="button" id="agregarHor" onclick=addHorario(this.data)> + </button></td></tr>')
                         $('#cantidad_hor').prop('value',1);
                         $("#enviar_am").prop('disabled',false)
                     }else{
@@ -50,6 +53,7 @@ $(document).ready(function() {
             //alert("Debe seleccionar una materia")
             $(".horarios").remove()
             $(".horariosl").remove()
+            $("#agregarHor").remove()
             $("#agregarMat_mb").append('<div class="alert alert-error error_am"><button type="button" class="close" data-dismiss="alert">&times;</button>Debe seleccionar una materia</div>')
             $("#pg_am").hide()
         }
@@ -58,9 +62,30 @@ $(document).ready(function() {
     $( "#salir_am" ).click(function() {
         $(".horarios").remove()
         $(".horariosl").remove()
+        $("#agregarHor").remove()
         $(".error_am").remove()
         $('#id_materia').prop('selectedIndex',0);
         $('#id_aula').prop('selectedIndex',0);
 
     });
+
+    
 });
+
+function addHorario (data) {
+    if(data.length > 0){
+        $("#agregarMat").append('<tr><th><label id="horario1" class="horariosl" for="horarios">Horario: </label></th><td><select class="horarios" name="horario1">')
+        for (var i = 0;i < data.length;i++){
+            d = data[i]
+            //alert(d.dia_semana+" "+d.hora_inicio+" "+ d.hora_fin)
+            //alert('<option value="'+i+'">'+d.dia_semana+" "+d.hora_inicio+" "+ d.hora_fin+'</option>')
+            $(".horarios").append('<option value="'+ d.valor+'">'+d.dia_semana+" "+d.hora_inicio+"-"+ d.hora_fin+'</option>')
+        }
+        $("#agregarMat").append('</select></td></tr>')
+        $('#cantidad_hor').prop('value',1);
+        $("#enviar_am").prop('disabled',false)
+    }else{
+        $("#agregarMat_mb").append('<div class="alert alert-error error_am"><button type="button" class="close" data-dismiss="alert">&times;</button>La materia seleccionada no posee horarios asignados</div>')
+        $("#enviar_am").prop('disabled',true)
+    }
+}
