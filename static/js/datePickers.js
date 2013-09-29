ac505912a4ce3993149f249fd47aaf378fb089eb
Rafael Machado <mchdr19@gmail.com>
2013-09-29 17:43:37 -0430

    BugFix Timepicker

--- static/js/datePickers.js
+++ static/js/datePickers.js
@@ -1,8 +1,19 @@
  $(function() {
 
-
-$("#id_fecha_inicio").addClass("datepicker");
-$( "#id_fecha_fin" ).addClass("datepicker");
+$("input[id^='id_fecha']").addClass("datepicker");
+//$( "#id_fecha_fin" ).addClass("datepicker");
 $( ".datepicker" ).datepicker({'format' : 'yyyy-mm-dd'});
+
+
+ $("input[id^='id_hora']" ).attr( "data-format", "hh:mm:ss" );
+ $("input[id^='id_hora']").wrap("<div id='hora' class='input-append hora'/div>");
+ $(".hora").append("<span class='add-on'><i data-time-icon='icon-time'></i></span>");
+
+
+$('.hora').datetimepicker({
+    pickDate: false,
+	pick12HourFormat: true,   // enables the 12-hour format time picker
+});
+
 
 });
